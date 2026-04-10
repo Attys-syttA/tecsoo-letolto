f73b683e4996fc7f3895b5785cacc06fcc6cf931
Attys-syttA <biro.attys@gmail.com>
2026-04-10 19:09:40 +0200

    Hide GUI console and fix batch comments

--- run.bat
+++ run.bat
@@ -1,13 +1,12 @@
 @echo off
 setlocal
 
-chcp 65001 >nul
 set "PYTHONUTF8=1"
 
-REM Opcionális: állítsd át, ha máshol van az ffmpeg
+REM Optional: change if ffmpeg is elsewhere
 set "FFMPEG_DIR=E:\ffmpeg-2026-04-09-git-d3d0b7a5ee-essentials_build\bin"
 
-REM Indítás: ha nincs paraméter, a program bekéri a linket
+REM If no args, the program will ask for the link
 py -3 "%~dp0letolto.py" %*
 set EXITCODE=%ERRORLEVEL%
 
--- run_gui.bat
+++ run_gui.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal
 
-chcp 65001 >nul
 set "PYTHONUTF8=1"
 
-REM Opcionális: állítsd át, ha máshol van az ffmpeg
+REM Optional: change if ffmpeg is elsewhere
 set "FFMPEG_DIR=E:\ffmpeg-2026-04-09-git-d3d0b7a5ee-essentials_build\bin"
 
-py -3 "%~dp0letolto_gui.py"
-endlocal
+REM Run GUI without keeping the console window open
+start "" /b py -3 -w "%~dp0letolto_gui.py"
+endlocal & exit /b 0
